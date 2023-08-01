33650b06fab39c4b6a76c2f04251edcbb200ae8f
KintalLam <KintalLam@hotmail.com>
2023-08-01 21:55:18 +0800

    1 Aug., week2, Go Gin入门

---
+++ webook/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	server.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Hello, world")
+	})
+	server.Run(":8080")
+}
